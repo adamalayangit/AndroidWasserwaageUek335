b664a626243df15ebd1e0cd778b3303a3e136829
Adam Alayan <adam.alayan@swisscom.com>
2021-01-18 10:49:16 +0100

    make vibration stop onPause of the application

--- app/src/main/java/com/example/androidwasserwaage/MainActivity.java
+++ app/src/main/java/com/example/androidwasserwaage/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.androidwasserwaage;
 
-import androidx.annotation.ColorRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -15,19 +11,17 @@ import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
-import android.os.Vibrator;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sensorManager;
     private Sensor rotationVectorSensor;
-    Vibrator vibrate;
+    private Vibrator vibrate;
 
 
 
@@ -35,13 +29,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-
         vibrate = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_NORMAL);
-
     }
 
     @Override
@@ -49,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         TextView x = findViewById(R.id.x);
         TextView y = findViewById(R.id.y);
         Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
         DecimalFormat twoDForm = new DecimalFormat("#.##");
         Float xFloat = Float.valueOf(twoDForm.format(event.values[0]));
         //Math.toDegrees(xFloat);
@@ -59,10 +53,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             View view = this.getWindow().getDecorView();
             view.setBackgroundColor(Color.GREEN);
             // notification
-            Toast.makeText(this, "The surface is even!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "The surface is even!", Toast.LENGTH_SHORT).show();
             // vibration
             vibrate();
-
         } else {
             View view = this.getWindow().getDecorView();
             view.setBackgroundColor(Color.WHITE);
@@ -74,29 +67,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onPause() {
         super.onPause();
-        vibrate.cancel();  // cancel for example here
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,rotationVectorSensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onUserLeaveHint() {
+        super.onUserLeaveHint();
+        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        vibrate.cancel();   // or cancel here
     }
 
     public void vibrate() {
-        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        //Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         if (Build.VERSION.SDK_INT >= 26) {
             ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(150, VibrationEffect.EFFECT_TICK));
         } else {
-            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(150);
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(150, VibrationEffect.EFFECT_TICK));
         }
     }
 
-    private void convertToDegrees(float[] vector){
+    /*private void convertToDegrees(float[] vector){
         for (int i = 0; i < vector.length; i++){
             vector[i] = Math.round(Math.toDegrees(vector[i]));
         }
-    }
+    }*/
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
